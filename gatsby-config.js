536af8dd3ad0a72d6b308bfe57c9ea893474a38c
mugattarova <mugattarova@gmail.com>
2025-02-19 16:57:25 +0100

    Gatsby config amended

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,37 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-module.exports = {
-  siteMetadata: {
-    title: `Nelli Mugattarova`,
-    siteUrl: `personalwebsitenm.netlify.com`,
-  },
-  plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-postcss',
-    'gatsby-transformer-remark',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'img',
-        path: './static/img/',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'blog-posts',
-        path: './static/blog-posts/',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'projects',
-        path: './static/projects/',
-      },
-    },
-  ],
+export const siteMetadata = {
+  title: `Nelli Mugattarova`,
+  description: `A personal website and portfolio of Nelli Mugattarova, a software engineer.`,
 };
+export const plugins = [
+  'gatsby-plugin-image',
+  'gatsby-plugin-sitemap',
+  'gatsby-plugin-mdx',
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-postcss',
+  'gatsby-transformer-remark',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'img',
+      path: './static/img/',
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'blog-posts',
+      path: './static/blog-posts/',
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'projects',
+      path: './static/projects/',
+    },
+  },
+];
